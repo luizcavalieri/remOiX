2d893f22ec02b4ac22e14e5bb7f2a153d785a2a9
Luiz cavalieri <luiz.cavalieri@gmail.com>
2016-05-12 20:28:27 +1000

    Initial Commit

---
+++ RemitanceOiX/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  RemitanceOiX
+//
+//  Created by Luiz cavalieri on 12/05/2016.
+//  Copyright © 2016 The Interactive Hippo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ RemitanceOiX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RemitanceOiX
+//
+//  Created by Luiz cavalieri on 12/05/2016.
+//  Copyright © 2016 The Interactive Hippo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
